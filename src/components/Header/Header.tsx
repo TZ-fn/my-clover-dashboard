1129f85f72228aa8207357dc69009461814d7c9c
Tomek <tomekz@vp.pl>
2024-08-08 16:34:33 +0200

    feat: add the header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import mainLogo from "../../assets/mainLogo.webp";
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <header className={styles.mainContainer}>
+      <a href="/">
+        <div className={styles.mainLogoContainer}>
+          <img src={mainLogo} alt="My Clover Logo" />
+        </div>
+      </a>
+      <h1 className={styles.mainHeaderText}>🍀 Lista klientów 🍀</h1>
+    </header>
+  );
+}
