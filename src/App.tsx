7eac8592e42fb235ffbdbedc5fc068642e142023
Tomek <tomekz@vp.pl>
2024-08-09 15:24:41 +0200

    feat: create list-filtering component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import ListFiltering from "./components/ListFiltering/ListFiltering";
 
 function App() {
   return (
     <>
       <Header />
+      <ListFiltering />
     </>
   );
 }
---
+++ src/components/ListFiltering/ListFiltering.tsx
@@ -0,0 +1,10 @@
+export default function ListFiltering() {
+  return (
+    <div>
+      <input name="withChildren" type="checkbox" />
+      <input name="birthday" type="checkbox" />
+      <input name="indemnityMonth" type="checkbox" />
+      <input name="indemnityYear" type="checkbox" />
+    </div>
+  );
+}
