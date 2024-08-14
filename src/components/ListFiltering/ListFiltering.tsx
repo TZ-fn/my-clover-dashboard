636580df492c23cfa34dd52516134661e6ee5c8c
Tomek <tomekz@vp.pl>
2024-08-14 15:31:56 +0200

    feat: fix label's matching, improve the styling

--- src/components/ListFiltering/ListFiltering.tsx
+++ src/components/ListFiltering/ListFiltering.tsx
@@ -3,22 +3,25 @@ import styles from "./ListFiltering.module.scss";
 export default function ListFiltering() {
   return (
     <div className={styles.mainContainer}>
-      <label htmlFor="withChildren">
-        <input name="withChildren" type="checkbox" />
-        Klienci z dziećmi.
-      </label>
-      <label htmlFor="birthday">
-        <input name="birthday" type="checkbox" />
-        Klienci obchodzący urodziny.
-      </label>
-      <label htmlFor="indemnityMonth">
-        <input name="indemnityMonth" type="checkbox" />
-        Indemnity miesiąc.
-      </label>
-      <label htmlFor="indemnityYear">
-        <input name="indemnityYear" type="checkbox" />
-        Indemnity rok.
-      </label>
+      <h3>Filtrowanie klientów</h3>
+      <div className={styles.checkboxesContainer}>
+        <label htmlFor="withChildren">
+          <input id="withChildren" type="checkbox" />
+          Klienci z dziećmi
+        </label>
+        <label htmlFor="birthday">
+          <input id="birthday" type="checkbox" />
+          Klienci obchodzący urodziny
+        </label>
+        <label htmlFor="indemnityMonth">
+          <input id="indemnityMonth" type="checkbox" />
+          Indemnity miesiąc
+        </label>
+        <label htmlFor="indemnityYear">
+          <input id="indemnityYear" type="checkbox" />
+          Indemnity rok
+        </label>
+      </div>
     </div>
   );
 }
