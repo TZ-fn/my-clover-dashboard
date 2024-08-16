02b181c09a75b6e0a497c4dc399916274a024d2a
Tomek <tomekz@vp.pl>
2024-08-16 10:11:42 +0200

    feat: add checkboxes-hiding option, improve styling

--- src/components/ListFiltering/ListFiltering.tsx
+++ src/components/ListFiltering/ListFiltering.tsx
@@ -1,27 +1,38 @@
+import { useState } from "react";
 import styles from "./ListFiltering.module.scss";
 
 export default function ListFiltering() {
+  const [areCheckboxesVisible, setAreCheckboxesVisible] = useState(false);
+
   return (
     <div className={styles.mainContainer}>
-      <h3>Filtrowanie klientów</h3>
-      <div className={styles.checkboxesContainer}>
-        <label htmlFor="withChildren">
-          <input id="withChildren" type="checkbox" />
-          Klienci z dziećmi
-        </label>
-        <label htmlFor="birthday">
-          <input id="birthday" type="checkbox" />
-          Klienci obchodzący urodziny
-        </label>
-        <label htmlFor="indemnityMonth">
-          <input id="indemnityMonth" type="checkbox" />
-          Indemnity miesiąc
-        </label>
-        <label htmlFor="indemnityYear">
-          <input id="indemnityYear" type="checkbox" />
-          Indemnity rok
-        </label>
-      </div>
+      <h3>
+        Filtrowanie klientów{" "}
+        <button className={styles.toggleCheckboxesBtn} onClick={() => setAreCheckboxesVisible(!areCheckboxesVisible)}>
+          {areCheckboxesVisible ? "▲" : "▼"}
+        </button>
+      </h3>
+
+      {areCheckboxesVisible && (
+        <div className={styles.checkboxesContainer}>
+          <label htmlFor="withChildren">
+            <input id="withChildren" type="checkbox" />
+            Klienci z dziećmi
+          </label>
+          <label htmlFor="birthday">
+            <input id="birthday" type="checkbox" />
+            Klienci obchodzący urodziny
+          </label>
+          <label htmlFor="indemnityMonth">
+            <input id="indemnityMonth" type="checkbox" />
+            Indemnity miesiąc
+          </label>
+          <label htmlFor="indemnityYear">
+            <input id="indemnityYear" type="checkbox" />
+            Indemnity rok
+          </label>
+        </div>
+      )}
     </div>
   );
 }
