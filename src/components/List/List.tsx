03d0daa97cf91d328a02679872f05b2f104ccdbe
Tomek <tomekz@vp.pl>
2024-08-27 20:23:26 +0200

    feat: fix the table elements, add styling

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,21 +1,37 @@
 import { useContext } from "react";
 import ClientsDataContext from "../../context/ClientsDataContext.js";
+import styles from "./List.module.scss";
 
 export default function List() {
   const { clientsData } = useContext(ClientsDataContext);
 
   return (
-    <table>
-      {clientsData &&
-        clientsData.map((client) => {
-          return (
-            <div key={`${client.firstName}${client.lastName}`}>
-              <p>
-                {client.firstName} {client.lastName}
-              </p>
-            </div>
-          );
-        })}
-    </table>
+    <div className={styles.listContainer}>
+      <table className={styles.table}>
+        <thead className={styles.tableHead}>
+          <tr>
+            <th className={styles.tableHeadCell}>Imię i Nazwisko</th>
+            <th className={styles.tableHeadCell}>Data urodzenia</th>
+            <th className={styles.tableHeadCell}>Telefon</th>
+            <th className={styles.tableHeadCell}>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientsData &&
+            clientsData.map((client) => {
+              return (
+                <tr key={`${client.firstName}${client.lastName}`}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.dob}</td>
+                  <td> {client.phone}</td>
+                  <td> {client.email}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
   );
 }
