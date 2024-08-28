ff6266997589a036df5d693db191a2d441717cdf
Tomek <tomekz@vp.pl>
2024-08-28 14:23:07 +0200

    feat: add more data cells and styling

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
 import ClientsDataContext from "../../context/ClientsDataContext.js";
+import formatCurrency from "../../utilities/formatCurrency.js";
 import styles from "./List.module.scss";
 
 export default function List() {
@@ -7,31 +8,61 @@ export default function List() {
 
   return (
     <div className={styles.listContainer}>
-      <table className={styles.table}>
-        <thead className={styles.tableHead}>
-          <tr>
-            <th className={styles.tableHeadCell}>Imię i Nazwisko</th>
-            <th className={styles.tableHeadCell}>Data urodzenia</th>
-            <th className={styles.tableHeadCell}>Telefon</th>
-            <th className={styles.tableHeadCell}>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {clientsData &&
-            clientsData.map((client) => {
+      {!clientsData && <p className={styles.noData}>Brak danych klientów...</p>}
+      {clientsData && (
+        <table className={styles.table}>
+          <thead className={styles.tableHead}>
+            <tr>
+              <th className={styles.tableHeadCell}>Imię i Nazwisko</th>
+              <th className={styles.tableHeadCell}>Data urodzenia</th>
+              <th className={styles.tableHeadCell}>Telefon</th>
+              <th className={styles.tableHeadCell}>Adres</th>
+              <th className={styles.tableHeadCell}>Email</th>
+              <th className={styles.tableHeadCell}>Produkty</th>
+              <th className={styles.tableHeadCell}>Provider</th>
+              <th className={styles.tableHeadCell}>Term</th>
+              <th className={styles.tableHeadCell}>Suma Ubezpieczenia</th>
+              <th className={styles.tableHeadCell}>Indemnity</th>
+              <th className={styles.tableHeadCell}>Broker</th>
+              <th className={styles.tableHeadCell}>Dzieci</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientsData.map((client) => {
               return (
                 <tr key={`${client.firstName}${client.lastName}`}>
                   <td>
                     {client.firstName} {client.lastName}
                   </td>
                   <td>{client.dob}</td>
-                  <td> {client.phone}</td>
-                  <td> {client.email}</td>
+                  <td>{client.phone}</td>
+                  <td>
+                    {client.address.rest}, {client.address.city}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>{client.products}</td>
+                  <td>{client.providers}</td>
+                  <td>
+                    {client.term.startDate} - {client.term.endDate}
+                  </td>
+                  <td>{formatCurrency(Number(client.insuranceSum))}</td>
+                  <td>{client.indemnityTime}</td>
+                  <td>{client.broker}</td>
+                  <td>
+                    {client.children.map((children) => (
+                      <tr>
+                        <td>
+                          {children.firstName} {children.lastName}, {children.dob}
+                        </td>
+                      </tr>
+                    ))}
+                  </td>
                 </tr>
               );
             })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
