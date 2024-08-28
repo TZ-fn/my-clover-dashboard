27b0201d44edb0450528b4ad349471a28dc315ab
Tomek <tomekz@vp.pl>
2024-08-28 13:40:17 +0200

    feat: add a utility function for formatting the currency

---
+++ src/utilities/formatCurrency.ts
@@ -0,0 +1,7 @@
+export default function formatCurrency(amount: number) {
+  return new Intl.NumberFormat("en-GB", {
+    style: "currency",
+    currency: "GBP",
+    maximumFractionDigits: 0,
+  }).format(amount);
+}
