54bf4c383d2cea0e38b8dfc0619f1168192c48a9
Tomek <tomekz@vp.pl>
2024-08-30 20:23:38 +0200

    feat: add list filtering, WIP

--- src/components/ListFiltering/ListFiltering.tsx
+++ src/components/ListFiltering/ListFiltering.tsx
@@ -1,8 +1,20 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import ClientsDataContext from "../../context/ClientsDataContext";
+import { Client } from "../../types/Client";
 import styles from "./ListFiltering.module.scss";
 
 export default function ListFiltering() {
   const [areCheckboxesVisible, setAreCheckboxesVisible] = useState(false);
+  const { clientsData, setClientsData } = useContext(ClientsDataContext);
+  const [filteredClientsData, setFilteredClientsData] = useState<Client[] | null>(null);
+
+  useEffect(() => {
+    setFilteredClientsData(clientsData);
+  }, [clientsData]);
+
+  function handleInputs(e) {
+    console.log(e.target.id);
+  }
 
   return (
     <div className={styles.mainContainer}>
@@ -16,19 +28,19 @@ export default function ListFiltering() {
       {areCheckboxesVisible && (
         <div className={styles.checkboxesContainer}>
           <label htmlFor="withChildren">
-            <input id="withChildren" type="checkbox" />
+            <input onChange={(e) => handleInputs(e)} id="withChildren" type="checkbox" />
             Klienci z dziećmi
           </label>
           <label htmlFor="birthday">
-            <input id="birthday" type="checkbox" />
+            <input onChange={(e) => handleInputs(e)} id="birthday" type="checkbox" />
             Klienci obchodzący dziś urodziny
           </label>
           <label htmlFor="indemnityMonth">
-            <input id="indemnityMonth" type="checkbox" />
+            <input onChange={(e) => handleInputs(e)} id="indemnityMonth" type="checkbox" />
             Indemnity miesiąc
           </label>
           <label htmlFor="indemnityYear">
-            <input id="indemnityYear" type="checkbox" />
+            <input onChange={(e) => handleInputs(e)} id="indemnityYear" type="checkbox" />
             Indemnity rok
           </label>
         </div>
