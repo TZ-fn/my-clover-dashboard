1dc5ef5c023abce92e96186a9ebfd6cbd9fd1f71
Tomek <tomekz@vp.pl>
2024-09-16 18:14:33 +0200

    fix: use the imported type

--- src/context/ListFilteringContext.ts
+++ src/context/ListFilteringContext.ts
@@ -3,14 +3,10 @@ import { createContext } from "react";
 
 const ListFilteringContext = createContext<{
   listFiltering: ListFilteringSettings;
-  setListFiltering?: React.Dispatch<
-    React.SetStateAction<{
-      searchQuery: string;
-      clientsChildren: boolean;
-      clientsBirthDay: boolean;
-      currentPage: number;
-    }>
-  >;
-}>({ listFiltering: { searchQuery: "", clientsChildren: false, clientsBirthDay: false, currentPage: 1 } });
+  setListFiltering: React.Dispatch<React.SetStateAction<ListFilteringSettings>> | null;
+}>({
+  listFiltering: { searchQuery: "", clientsChildren: false, clientsBirthDay: false, currentPage: 1, numberOfPages: 1 },
+  setListFiltering: null,
+});
 
 export default ListFilteringContext;
